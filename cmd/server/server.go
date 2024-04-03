52c5eeaf6ec3ab74af68a71b300871b6cde8a82d
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-04-03 21:51:00 +0500

    server runner function

---
+++ cmd/server/server.go
@@ -0,0 +1,34 @@
+package server
+
+import (
+	"context"
+	"net/http"
+	"time"
+)
+
+type Server struct {
+	httpServer *http.Server
+}
+
+func (s *Server) Run(port string, handler http.Handler) error {
+
+	s.httpServer = &http.Server{
+		Addr:           ":" + port,
+		Handler:        handler,
+		MaxHeaderBytes: 1 << 20,
+		WriteTimeout:   5 * time.Second,
+		ReadTimeout:    5 * time.Second,
+	}
+
+	return s.httpServer.ListenAndServe()
+}
+
+func (s *Server) Shutdown(ctx context.Context) error {
+	return s.httpServer.Shutdown(ctx)
+}
+
+func (s *Server) MustShutdown(ctx context.Context) {
+	if err := s.Shutdown(ctx); err != nil {
+		panic(err)
+	}
+}
