b073629266045058d16ab0feb10aa412b038bff2
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-04-03 21:54:51 +0500

    changed token type

--- migrations/000001_init_schema.up.sql
+++ migrations/000001_init_schema.up.sql
@@ -1,12 +1,11 @@
-CREATE TABLE users(
-    id         BIGSERIAL PRIMARY KEY,
-    name       VARCHAR(255) NOT NULL,
-    email      VARCHAR(255) NOT NULL,
-    number     VARCHAR(255) NOT NULL,
-    date_of_birth DATE NOT NULL,
-    user_type   VARCHAR(255) NOT NULL,
-    password   VARCHAR(255) NOT NULL,
-    token      VARCHAR(255) NOT NULL,
-    refresh_token VARCHAR(255) NOT NULL
+CREATE TABLE users (
+    id             BIGSERIAL PRIMARY KEY,
+    name           VARCHAR(255) NOT NULL,
+    email          VARCHAR(255) NOT NULL UNIQUE,
+    number         VARCHAR(255) NOT NULL,
+    date_of_birth  DATE NOT NULL,
+    user_type      VARCHAR(255) NOT NULL,
+    password       VARCHAR(255) NOT NULL,
+    token          TEXT NOT NULL,
+    refresh_token  TEXT NOT NULL
 );
-
