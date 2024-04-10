4ab0b1a188fd876b3c68919f1ddcebbe57522bc5
ctuzelov <to.the.love.03@gmail.com>
2024-04-11 02:02:32 +0500

    global middleware to get token

--- internal/handler/middleware.go
+++ internal/handler/middleware.go
@@ -1,11 +1,33 @@
 package handler
 
-import "github.com/gin-gonic/gin"
+import (
+	"net/http"
+	"ozinshe/internal/models"
+	"ozinshe/util"
 
-func (h *Handler) IsAuthorized(c *gin.Context) {
+	"github.com/gin-gonic/gin"
+)
+
+func (h *Handler) Middleware(c *gin.Context) {
 	token, err := c.Cookie("token")
+	data := &Data{}
 
-	
+	switch err {
+	case http.ErrNoCookie:
+		data.User = models.User{}
+		data.IsAuthorized = false
+		data.IsAdmin = false
+	case nil:
+		validToken, err := util.ValidateToken(token)
+		if err != nil {
+			h.errorpage(c, http.StatusBadRequest, err, "token validation failed")
+		}
+		data.User.Email, data.User.UserType, data.User.Name = validToken["email"].(string), validToken["user_type"].(string), validToken["name"].(string)
+		data.IsAuthorized = true
+		data.IsAdmin = data.User.UserType == "admin"
+	}
+
+	c.Set("data", data)
 
 	c.Next()
 }
