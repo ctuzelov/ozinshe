4b982e96f47468bdb33c4a63262f0117938ff5b6
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-04-24 16:32:53 +0500

    fix: deleting the user by id

--- internal/storage/postgresql/user_psql.go
+++ internal/storage/postgresql/user_psql.go
@@ -104,3 +104,97 @@ func (u *UserStorage) DeleteTokens(ctx context.Context, email string) error {
 
 	return nil
 }
+
+func (u *UserStorage) GetAll(ctx context.Context) ([]models.User, error) {
+	const op = "storage.user.GetAll"
+
+	stmt, err := u.storage.db.Prepare("SELECT id, name, email, number, date_of_birth, user_type, password FROM users")
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	rows, err := stmt.QueryContext(ctx)
+	if err != nil {
+		return nil, fmt.Errorf("%s: %w", op, err)
+	}
+
+	var users []models.User
+	for rows.Next() {
+		var user models.User
+		if err := rows.Scan(&user.ID, &user.Name, &user.Email, &user.Number, &user.DateOfBirth, &user.UserType, &user.Password); err != nil {
+			return nil, fmt.Errorf("%s: %w", op, err)
+		}
+		users = append(users, user)
+	}
+
+	return users, nil
+}
+
+func (u *UserStorage) GetById(ctx context.Context, id int) (models.User, error) {
+	const op = "storage.user.GetById"
+
+	stmt, err := u.storage.db.Prepare("SELECT id, name, email, number, date_of_birth, user_type, password FROM users WHERE id = $1")
+	if err != nil {
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	row := stmt.QueryRowContext(ctx, id)
+
+	var user models.User
+	if err := row.Scan(&user.ID, &user.Name, &user.Email, &user.Number, &user.DateOfBirth, &user.UserType, &user.Password); err != nil {
+		if errors.Is(err, sql.ErrNoRows) {
+			return models.User{}, fmt.Errorf("%s: %w", op, storage.ErrUserNotFound)
+		}
+		return models.User{}, fmt.Errorf("%s: %w", op, err)
+	}
+
+	return user, nil
+}
+
+func (u *UserStorage) ChangePassword(ctx context.Context, user models.User) error {
+	const op = "storage.user.ChangePassword"
+
+	stmt, err := u.storage.db.Prepare("UPDATE users SET password = $1 WHERE email = $2")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.ExecContext(ctx, user.Password, user.Email)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (u *UserStorage) ChangeProfileData(ctx context.Context, user models.User) error {
+	const op = "storage.user.ChangeProfileData"
+
+	stmt, err := u.storage.db.Prepare("UPDATE users SET name = $1, number = $2, date_of_birth = $3 WHERE email = $4")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.ExecContext(ctx, user.Name, user.Number, user.DateOfBirth, user.Email)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
+
+func (u *UserStorage) Delete(ctx context.Context, id int) error {
+	const op = "storage.user.Delete"
+
+	stmt, err := u.storage.db.Prepare("DELETE FROM users WHERE id = $1")
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	_, err = stmt.ExecContext(ctx, id)
+	if err != nil {
+		return fmt.Errorf("%s: %w", op, err)
+	}
+
+	return nil
+}
