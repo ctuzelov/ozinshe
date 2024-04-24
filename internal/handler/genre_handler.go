9a2ae4b7808a11fbb2f3f0b21395f4e60b107dbc
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-04-24 16:15:32 +0500

    add: genre handlers

--- internal/handler/genre_handler.go
+++ internal/handler/genre_handler.go
@@ -2,6 +2,8 @@ package handler
 
 import (
 	"net/http"
+	"ozinshe/internal/models"
+	"strconv"
 
 	"github.com/gin-gonic/gin"
 )
@@ -11,17 +13,51 @@ func (h *Handler) GenresPage(c *gin.Context) {
 }
 
 func (h *Handler) GetAllGenres(c *gin.Context) {
-	h.render(c, http.StatusOK, "genres.html", nil)
+	genres, err := h.Service.Genre.GetAll()
+
+	if err != nil {
+		h.errorpage(c, http.StatusBadRequest, err, "genres getting failed")
+		return
+	}
+
+	c.JSON(http.StatusOK, genres)
 }
 
 func (h *Handler) GetGenre(c *gin.Context) {
-	h.render(c, http.StatusOK, "genres.html", nil)
+	id, _ := strconv.Atoi(c.Param("id"))
+	genre, err := h.Service.Genre.GetById(id)
+
+	if err != nil {
+		h.errorpage(c, http.StatusBadRequest, err, "genre getting failed")
+		return
+	}
+
+	c.JSON(http.StatusOK, genre)
 }
 
 func (h *Handler) CreateGenre(c *gin.Context) {
-	h.render(c, http.StatusOK, "genres.html", nil)
+
+	form, err := c.MultipartForm()
+	if err != nil {
+		h.errorpage(c, http.StatusBadRequest, err, "binding multipart form in create genre")
+		return
+	}
+
+	genres := form.Value["genre"]
+
+	err = h.Service.Genre.Add(append([]models.Genre{}, models.Genre{Name: genres[0]}))
+	if err != nil {
+		h.errorpage(c, http.StatusBadRequest, err, "genre creation failed")
+		return
+	}
 }
 
 func (h *Handler) DeleteGenre(c *gin.Context) {
-	h.render(c, http.StatusOK, "genres.html", nil)
+	id, _ := strconv.Atoi(c.Param("id"))
+	err := h.Service.Genre.Remove(id)
+	if err != nil {
+		h.errorpage(c, http.StatusBadRequest, err, "genre deleting failed")
+		return
+	}
+	c.JSON(http.StatusOK, gin.H{"message": "Genre deleted"})
 }
