f950b22525a6e90e24de8d3104530f632eaa7c62
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-04-24 16:25:30 +0500

    add: crud operations in service

--- internal/service/age_category_service.go
+++ internal/service/age_category_service.go
@@ -20,3 +20,21 @@ func (a *AgeCategoryService) Add(age_category []models.AgeCategory) error {
 	defer cancel()
 	return a.AgeCategory.Insert(ctx, age_category)
 }
+
+func (a *AgeCategoryService) Remove(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return a.AgeCategory.Delete(ctx, id)
+}
+
+func (a *AgeCategoryService) GetById(id int) (models.AgeCategory, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return a.AgeCategory.GetById(ctx, id)
+}
+
+func (a *AgeCategoryService) GetAll() ([]models.AgeCategory, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+	return a.AgeCategory.GetAll(ctx)
+}
--- internal/service/genre_service.go
+++ internal/service/genre_service.go
@@ -21,3 +21,24 @@ func (g *GenreService) Add(genre []models.Genre) error {
 
 	return g.Genre.Insert(ctx, genre)
 }
+
+func (g *GenreService) Remove(id int) error {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	return g.Genre.Delete(ctx, id)
+}
+
+func (g *GenreService) GetById(id int) (models.Genre, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	return g.Genre.GetById(ctx, id)
+}
+
+func (g *GenreService) GetAll() ([]models.Genre, error) {
+	ctx, cancel := context.WithTimeout(context.Background(), 10*time.Second)
+	defer cancel()
+
+	return g.Genre.GetAll(ctx)
+}
