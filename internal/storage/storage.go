593b860b39300e19926bfaf8bed4e592bcce4a80
ctuzelov <chingizkan.tuzelov@gmail.com>
2024-04-24 16:28:55 +0500

    add: new errors

--- internal/storage/storage.go
+++ internal/storage/storage.go
@@ -5,4 +5,6 @@ import "fmt"
 var (
 	ErrUserNotFound = fmt.Errorf("user not found")
 	ErrUserExists   = fmt.Errorf("user already exists")
+	ErrMovieExists  = fmt.Errorf("movie already exists")
+	ErrSeriesExists = fmt.Errorf("series already exists")
 )
